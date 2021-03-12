92aa23f3e794af1f22863dda389ffcbc19b4f673
Rasmus Kyyhkynen <rojkyy@utu.fi>
2021-03-12 21:16:02 +0200

    some buttons

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function goLinkedin() {
+    window.location("www.linkedin.com/in/rasmus-kyyhkynen")
+}
+
+function goGithub() {
+  window.location("https://github.com/rkyyhkynen")
+}
+
+function goInstagram() {
+  window.location("https://www.instagram.com/rkyyhky/")
+}
